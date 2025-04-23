340657303529048faa06d5884c8eaa8aab5b61de
Costin Bernhart <costin.bernhart@tuwien.ac.at>
2025-04-23 17:47:04 -0300

    fixed css layer styling of buttons

--- client/packages/big-hello-world/src/browser/MainView.tsx
+++ client/packages/big-hello-world/src/browser/MainView.tsx
@@ -9,64 +9,75 @@
 import { VSCodeButton, VSCodeDivider } from '@vscode/webview-ui-toolkit/react/index.js';
 import type { Layer } from '../model/model.js';
 
-export function MainView({ layers, moveUp, moveDown, toggleActive, goToDetails, uploadConfig, saveConfig, recomputeAll, addLayer }: {
-  layers: Layer[];
-  moveUp: (id: string) => void;
-  moveDown: (id: string) => void;
-  toggleActive: (id: string) => void;
-  goToDetails: (id: string) => void;
-  uploadConfig: () => void;
-  saveConfig: () => void;
-  recomputeAll: () => void;
-  addLayer: () => void;
+export function MainView({
+    layers,
+    moveUp,
+    moveDown,
+    toggleActive,
+    goToDetails,
+    uploadConfig,
+    saveConfig,
+    recomputeAll,
+    addLayer
+}: {
+    layers: Layer[];
+    moveUp: (id: string) => void;
+    moveDown: (id: string) => void;
+    toggleActive: (id: string) => void;
+    goToDetails: (id: string) => void;
+    uploadConfig: () => void;
+    saveConfig: () => void;
+    recomputeAll: () => void;
+    addLayer: () => void;
 }) {
-  return (
-    <div id="main-view">
-      <div id="layer-list">
-        <h2>Your Layers</h2>
-        {layers.map(layer => (
-          <div key={layer.id} id="layer">
-            <span>{layer.name}</span>
-            <div id="layer-buttons">
-              <VSCodeButton slot='anchor' appearance='icon' onClick={() => moveUp(layer.id)}>
-                <div className='codicon codicon-chevron-up'></div>
-              </VSCodeButton>
-              <VSCodeButton slot='anchor' appearance='icon' onClick={() => moveDown(layer.id)}>
-                <div className='codicon codicon-chevron-down'></div>
-              </VSCodeButton>
-              <VSCodeButton slot='anchor' appearance='icon' onClick={() => toggleActive(layer.id)}>
-                {layer.visible
-                  ? <div className='codicon codicon-eye'></div>
-                  : <div className='codicon codicon-eye-closed'></div>
-                }
-              </VSCodeButton>
-              <VSCodeButton slot='anchor' appearance='icon' onClick={() => goToDetails(layer.id)}>
-                <div className='codicon codicon-chevron-right'></div>
-              </VSCodeButton>
+    return (
+        <div id='main-view'>
+            <div id='layer-list'>
+                <h2>Your Layers</h2>
+                {layers.map(layer => (
+                    <div key={layer.id} id='layer'>
+                        <span>{layer.name}</span>
+                        <div id='layer-buttons' style={{ display: 'flex', flexDirection: 'row', gap: '4px', alignItems: 'center' }}>
+                            <VSCodeButton slot='anchor' appearance='icon' onClick={() => moveUp(layer.id)}>
+                                <div className='codicon codicon-chevron-up'></div>
+                            </VSCodeButton>
+                            <VSCodeButton slot='anchor' appearance='icon' onClick={() => moveDown(layer.id)}>
+                                <div className='codicon codicon-chevron-down'></div>
+                            </VSCodeButton>
+                            <VSCodeButton slot='anchor' appearance='icon' onClick={() => toggleActive(layer.id)}>
+                                {layer.visible ? (
+                                    <div className='codicon codicon-eye'></div>
+                                ) : (
+                                    <div className='codicon codicon-eye-closed'></div>
+                                )}
+                            </VSCodeButton>
+                            <VSCodeButton slot='anchor' appearance='icon' onClick={() => goToDetails(layer.id)}>
+                                <div className='codicon codicon-chevron-right'></div>
+                            </VSCodeButton>
+                        </div>
+                    </div>
+                ))}
             </div>
-          </div>
-        ))}
-      </div>
-      <br />
-      <VSCodeDivider />
-      <footer>
-        <div id="secondary-footer-buttons">
-          <VSCodeButton slot='anchor' appearance='icon' onClick={uploadConfig}>
-            <div className='codicon codicon-cloud-upload'></div>
-          </VSCodeButton>
-          <VSCodeButton slot='anchor' appearance='icon' onClick={saveConfig}>
-            <div className='codicon codicon-cloud-download'></div>
-          </VSCodeButton>
-          <VSCodeButton slot='anchor' appearance='icon' onClick={recomputeAll}>
-            <div className='codicon codicon-refresh'></div>
-          </VSCodeButton>
+            <br />
+            <VSCodeDivider />
+            <footer>
+                <div id='secondary-footer-buttons' style={{ display: 'flex', flexDirection: 'row', gap: '4px', alignItems: 'center' }}>
+                    <VSCodeButton slot='anchor' appearance='icon' onClick={uploadConfig}>
+                        <div className='codicon codicon-cloud-upload'></div>
+                    </VSCodeButton>
+                    <VSCodeButton slot='anchor' appearance='icon' onClick={saveConfig}>
+                        <div className='codicon codicon-cloud-download'></div>
+                    </VSCodeButton>
+                    <VSCodeButton slot='anchor' appearance='icon' onClick={recomputeAll}>
+                        <div className='codicon codicon-refresh'></div>
+                    </VSCodeButton>
+                </div>
+                <div id='primary-footer-button'>
+                    <VSCodeButton slot='anchor' appearance='icon' onClick={addLayer}>
+                        <div className='codicon codicon-add'></div>
+                    </VSCodeButton>
+                </div>
+            </footer>
         </div>
-        <div id="primary-footer-button">
-          <VSCodeButton slot='anchor' appearance='icon' onClick={addLayer}>
-            <div className='codicon codicon-add'></div>
-          </VSCodeButton>
-        </div>
-      </footer>
-    </div>
-  );
+    );
 }
